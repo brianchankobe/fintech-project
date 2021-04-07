92ae582f4e27952747ea78336584f7bf515cbb06
CHAN YEUNG YEH <20403496@life.hkbu.edu.hk>
2021-04-07 18:41:33 +0800

    stock search part unfinished

--- api/controllers/StockController.js
+++ api/controllers/StockController.js
@@ -5,12 +5,28 @@
  * @help        :: See https://sailsjs.com/docs/concepts/actions
  */
 
+const finnhub = require('finnhub');
+const api_key = finnhub.ApiClient.instance.authentications['api_key'];
+api_key.apiKey = "c0n56d748v6v1q0c1ph0"; // Replace this
+const finnhubClient = new finnhub.DefaultApi();
+
+
 module.exports = {
 
     // action - create
     search: async function (req, res) {
 
         if (req.method == "GET") return res.view('stock/search');
+
+        if (req.wantsJSON) {  //ajax
+            var symbol_res = finnhubClient.stockSymbols(req.body.exchange, (error, data, response) => {
+                return res.json(data);
+            });
+
+            
+        } else {
+            res.redirect("/stock/search")
+        }
 
         /*if (req.wantsJSON) { //ajax call
 
