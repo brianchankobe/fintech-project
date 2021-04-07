bc9e566e6827e9153d6f125f8a56ce1949d41d2d
jason <jason.@jasondeMacBook-Pro.local>
2021-04-07 20:33:15 +0800

    detail page (3) (add the stock profile information)

--- api/controllers/StockController.js
+++ api/controllers/StockController.js
@@ -23,16 +23,22 @@ module.exports = {
 
   // action - create
   detail: async function (req, res) {
-    if (req.method == "GET") return res.view("stock/detail");
+    // const fetch = require("node-fetch");
+    if (req.method == "GET") {
+      //   //fetch company profile
+      //   async function stockProfile(symbol) {
+      //     const response = await fetch(
+      //       "https://finnhub.io/api/v1/stock/profile2?symbol=" +
+      //         symbol +
+      //         "&token=c0ro5sv48v6voi2d8ndg"
+      //     );
+      //     let data = await response.json();
+      //     console.log(data);
+      //   }
+      //   // stockProfile("BINANCE:BTCUSDT");
+      //   stockProfile("AAPL");
 
-    /*if (req.wantsJSON) { //ajax call
-
-            var stock = await Stock.create(req.body).fetch();
-
-            //status 201: created
-            return res.status(201).json({ id: stock.id });
-        } else { // normal call
-            res.redirect('/stock/search');
-        }*/
+      return res.view("stock/detail");
+    }
   },
 };
