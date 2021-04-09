62e17cde2a8f66fb8624fe49c23fc08e618bd0e2
CHAN YEUNG YEH <20403496@life.hkbu.edu.hk>
2021-04-10 02:14:07 +0800

    stock search page output stock

--- api/controllers/StockController.js
+++ api/controllers/StockController.js
@@ -8,35 +8,25 @@
 const finnhub = require('finnhub');
 const api_key = finnhub.ApiClient.instance.authentications['api_key'];
 api_key.apiKey = "c0n56d748v6v1q0c1ph0"; // Replace this
-const finnhubClient = new finnhub.DefaultApi();
+//const finnhubClient = new finnhub.DefaultApi();
 
+//const request = require('request');
 
 module.exports = {
 
     // action - create
     search: async function (req, res) {
 
-        if (req.method == "GET") return res.view('stock/search');
+        if (req.method == "GET") return res.view('stock/search', {my_api: api_key.apiKey});
 
-        if (req.wantsJSON) {  //ajax
-            var symbol_res = finnhubClient.stockSymbols(req.body.exchange, (error, data, response) => {
-                return res.json(data);
-            });
+        /*if (req.wantsJSON) {  //ajax
 
-            
+            //get all related symbols from exchange code
+            //var data = finnhubClient.stockSymbols(req.body.exchange)
         } else {
-            res.redirect("/stock/search")
-        }
-
-        /*if (req.wantsJSON) { //ajax call
-
-            var stock = await Stock.create(req.body).fetch();
-
-            //status 201: created
-            return res.status(201).json({ id: stock.id });
-        } else { // normal call
-            res.redirect('/stock/search');
+            res.redirect("/stock/search");
         }*/
+
     },
 
 };
